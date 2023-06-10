1201f55ce693fef91b2f0d2d71561defa5360c33
Jo Carr <hulaus21@gmail.com>
2023-06-10 01:41:20 -0700

    added public directory w/ index.html with starter code. Installed html webpack plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Rick and Morty');
+// console.log('Rick and Morty');  This was the initial test for the index.js
+import ReactDOM from 'react-dom/client';
+
+function App() {
+    return <h1>Rick & Morty</h1>
+}
+
+const container = document.getElementById('app');
+const root = ReactDOM.createRoot(container);
+root.render(<App />); //Mounts the App component to an element with the root ID selector in my document, which is the entry point of the application. 
