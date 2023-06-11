75ca3ec7e0a48d7c1f26020ccb1eb95c65b63467
Jo Carr <hulaus21@gmail.com>
2023-06-10 21:51:50 -0700

    changed app inside of const container = document.getElementbyID(app) to root and I can now see the application

---
+++ src/components/Character.js
@@ -0,0 +1,7 @@
+function Character() {
+
+    return <h3>Character</h3>;
+  
+  };
+  
+  export default Character;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 // console.log('Rick and Morty');  This was the initial test for the index.js
 import ReactDOM from 'react-dom/client';
+import List from './components/List';
 
 function App() {
-    return <h1>Rick & Morty</h1>
+    return (
+        <div>
+            <h1>Rick & Morty</h1>
+            <List />
+        </div>
+    )
 }
 
-const container = document.getElementById('app');
+const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 root.render(<App />); //Mounts the App component to an element with the root ID selector in my document, which is the entry point of the application. 
