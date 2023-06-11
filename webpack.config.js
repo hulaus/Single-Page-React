438683c134a27c0b1bcfe21ccd9bcd8263389f43
Jo Carr <hulaus21@gmail.com>
2023-06-11 00:42:08 -0700

    restarted vscode and react app now works. Was getting an error where it was not paring correctly

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,11 @@ module.exports = {
 
     test: /\.css$/,
         
-    use: ['style-loader', 'css-loader', 'postcss-loader'],
+    use: [{
+        loader: 'style-loader'
+    }, {
+        loader: 'css-loader'
+    }],
         
     },
     
