b0198229f629cd6dc358ce0db7bc2b0afe68ac11
Jo Carr <hulaus21@gmail.com>
2023-06-10 23:16:36 -0700

    added useEffect and useState to fetch data from rick and morty API. Title, Pictures, Name and origin are now all visible

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,7 +1,13 @@
-function Character() {
 
-    return <h3>Character</h3>;
-  
-  };
-  
-  export default Character;
+
+function Character(character) {
+    return(
+        <div>
+            <h3>{ character.name }</h3>
+            <img src={character.image} alt={ character.name } width='300' />
+            <p> { `Origin: ${character.origin && character.origin.name}` } </p> 
+        </div>
+    );
+};
+
+export default Character;
--- src/components/List.js
+++ src/components/List.js
@@ -1,11 +1,40 @@
 import Character from "./Character";
+import { useState, useEffect } from "react";
 
 function List() {
+
+    const [loading, setLoading] = useState(true);
+
+    const [characters, setCharacters] = useState([]); // Empty Array will be a placeholder for characters
+    useEffect(() => {
+        async function fetchData() {
+            const data = await fetch(
+                'https://rickandmortyapi.com/api/character'
+            );
+            const { results } = await data.json();
+            setCharacters(results);
+            setLoading(false);
+        }
+        fetchData();
+    }, [characters.length]); //The hook is only calling the logic to retrieve the data from the API when the length of the characters state changes.
+
+
     return (
         <div>
             <h2>Characters</h2>
-            <Character />
-            <Character />
+            {loading ? (
+                <div>Loading ... </div>
+            ) : (
+                characters.map((character) => (
+                    <Character
+                    key={character.id}
+                    name={character.name}
+                    origin={character.origin}
+                    image={character.image}
+                    
+                    />
+                ))
+            )}
         </div>
         )
 }
